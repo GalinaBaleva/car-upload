e289ff0c8eab2fed74a78d103b36791fc7d8b5f9
Galina <baleva1990@gmail.com>
2025-03-06 23:18:14 +0100

    Add static files

---
+++ public/index.js
@@ -0,0 +1,24 @@
+const form = document.forms.fileUpload
+form.addEventListener('submit', onSubmit)
+
+
+async function onSubmit(e) {
+    e.preventDefault()
+    const { fileInput, fileid } = form.elements
+    console.log(fileInput.files)
+    const file = fileInput.files[0]
+
+    const { name, size, type } = file
+    console.log({ name, size, type })
+
+    const id = fileid.value
+
+    const resp = await fetch('/cars/' + id, { method: 'PUT', body: file })
+    console.log(resp.headers.get('location'))
+    if (resp.status === 201) {
+        form.reset()
+
+        window.location.href = resp.url
+    }
+
+}
